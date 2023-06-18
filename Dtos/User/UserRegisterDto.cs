ad66266e2936f9abc780e77a0df06fbed0d31ab0
Konrad Droeske <kdroeske@gmail.com>
2023-06-18 08:52:02 -0400

    added register controller

---
+++ Dtos/User/UserRegisterDto.cs
@@ -0,0 +1,7 @@
+namespace PMFrameworks.Dtos.User;
+
+public class UserRegisterDto
+{
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 global using Microsoft.EntityFrameworkCore;
 global using PMFrameworks.Data;
+global using PMFrameworks.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +15,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IAuthRepository, AuthRepository>();
 
 var app = builder.Build();
 
