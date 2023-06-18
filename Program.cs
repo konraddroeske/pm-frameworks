6f9d00c62991ce92096e4391586ee30f646aa0e5
Konrad Droeske <kdroeske@gmail.com>
2023-06-18 10:39:26 -0400

    added auth to program

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ global using Microsoft.EntityFrameworkCore;
 global using PMFrameworks.Data;
 global using PMFrameworks.Models;
 using dotenv.net;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 DotEnv.Load();
 var builder = WebApplication.CreateBuilder(args);
@@ -19,6 +21,22 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IAuthRepository, AuthRepository>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(
+        o =>
+        {
+            o.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(
+                    System.Text.Encoding.UTF8.GetBytes(
+                        Environment.GetEnvironmentVariable("JWT_SECRET_KEY")!
+                    )
+                ),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+            };
+        });
 
 var app = builder.Build();
 
@@ -31,6 +49,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
